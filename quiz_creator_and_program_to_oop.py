c8e2d506775f5f0b5a53b6bfc894a8beca99dae0
talamor-shella <shellatalamor@gmail.com>
2025-05-29 05:31:04 +0800

    start taking the quiz, try to load questions, while loop while there are questions left, created variable that randomly picks question, if- else statement to check correct and incorrect answers

--- quiz_creator_and_program_to_oop.py
+++ quiz_creator_and_program_to_oop.py
@@ -106,5 +106,28 @@ class QuizTaker(QuizManager):
         super().__init__(category)
         self.correct_count = 0
         self.incorrect_count = 0
-        
+    
+    # start taking the quiz
+    def start_quiz_taker(self):
+        if not self.load_questions_from_file():  # Try to load questions
+            return
+        print("\n--- Quiz Taker ---")  
+        while self.questions:  
+            question = random.choice(self.questions)  # Picks a random question
+            correct = question.ask_user()  
+
+            if correct:  
+                print("Correct!\n")  
+                self.correct_count += 1  
+            else:
+                print(f"Wrong! The correct answer is {question.correct_answer}\n")  
+                self.incorrect_count += 1  
+
+            self.questions.remove(question)  # Remove the asked question
+
+            cont = input("Do you want to continue? (yes/no): ").lower()  
+            if cont != "yes": 
+                break
+
+    
 # define main function
