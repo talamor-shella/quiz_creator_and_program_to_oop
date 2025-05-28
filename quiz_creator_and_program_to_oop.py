ad3ed4a84819157706e7484ca58d183e6effa435
talamor-shella <shellatalamor@gmail.com>
2025-05-29 04:51:01 +0800

    load questions fomr a file for the selected category, for loop to process every 7 lines, gets the question text, get text after a,b,c and d, get correct answer, and append to the question list

--- quiz_creator_and_program_to_oop.py
+++ quiz_creator_and_program_to_oop.py
@@ -38,5 +38,27 @@ class QuizManager:
         self.category = category.lower()
         self.filename = self.category + ".txt"
         self.questioms = []
+        
+    # load questions from a file for the selected category
+    def load_questions_from_file(self):
+        try: 
+            with open(self.filename, "r") as file:
+                lines = file.readlines()
+            for i in range (0, len(lines), 7): # Process every 7 lines (1 question block)
+                try:
+                    text = lines[i + 1].strip()  # Get the question text
+                    choices = { 
+                        'a': lines[i + 2].strip().split(') ', 1)[1],  # Get text after "a) "
+                        'b': lines[i + 3].strip().split(') ', 1)[1],  # Get text after "b) "
+                        'c': lines[i + 4].strip().split(') ', 1)[1],  # Get text after "c) "
+                        'd': lines[i + 5].strip().split(') ', 1)[1],  # Get text after "d) "
+                    }
+                    correct = lines[i + 6].strip().replace("The correct answer is: ", "")  # Get correct answer
+                    self.questions.append(Question(text, choices, correct))  # Add to the question list
+                except IndexError:
+                    continue 
+        except FileNotFoundError:
+
+
 # define class quiz taker that inherits from quiz manager
 # define main function
