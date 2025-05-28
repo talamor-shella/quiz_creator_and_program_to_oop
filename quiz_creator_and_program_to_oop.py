4b6dc8928ad926ed3ac5e452e83f29a4f337eb30
talamor-shella <shellatalamor@gmail.com>
2025-05-29 04:05:23 +0800

    added pseudocode

--- quiz_creator_and_program_to_oop.py
+++ quiz_creator_and_program_to_oop.py
@@ -1,1 +1,7 @@
 #Assignment 11: Convert Assignment 9 and 10 to OOP
+
+#random module
+#define class question
+#define class quiz manager
+#define class quiz taker that inherits from quiz manager
+#define main function
