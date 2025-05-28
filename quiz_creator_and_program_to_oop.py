293877871e7b221004cf582b850dba140f9f56ba
talamor-shella <shellatalamor@gmail.com>
2025-05-29 04:06:57 +0800

    imported random module for randomizing quiz questions

--- quiz_creator_and_program_to_oop.py
+++ quiz_creator_and_program_to_oop.py
@@ -1,6 +1,8 @@
 #Assignment 11: Convert Assignment 9 and 10 to OOP
 
 #random module
+import random
+
 #define class question
 #define class quiz manager
 #define class quiz taker that inherits from quiz manager
